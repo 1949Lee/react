18e704449a60a599c693838b9d5b5f74c8748ae0
1949Lee <lijiaxuan0811@hotmail.com>
2019-06-09 19:11:03 +0800

    init

---
+++ React.JS/blog/src/index.js
@@ -0,0 +1,6 @@
+import React, {Fragment} from 'react';
+import ReactDOM from 'react-dom';
+import {GlobalStyle} from './style.js';
+import App from './App';
+
+ReactDOM.render(<Fragment><App/><GlobalStyle/></Fragment>, document.getElementById('root'));
