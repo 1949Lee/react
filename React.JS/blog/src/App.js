971060a78ea8052c038c069e49cdcd8ec1c66add
1949Lee <lijiaxuan0811@hotmail.com>
2019-07-03 23:11:10 +0800

    update

--- React.JS/blog/src/App.js
+++ React.JS/blog/src/App.js
@@ -2,15 +2,27 @@ import React from 'react';
 import Header from './common/header';
 import {Provider} from "react-redux";
 import store from "./store";
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./pages/home";
+import Article from "./pages/article";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <Header/>
+        <div style={headerBg}></div>
+        <BrowserRouter>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/article" component={Article} />
+        </BrowserRouter>
       </div>
     </Provider>
   );
 }
+
+const headerBg = {
+  height:'56px'
+};
 
 export default App;
