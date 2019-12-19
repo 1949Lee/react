ada79d8678d6ee2f8a93abcf6a24da3b630e08e3
lijiaxuan <lijiaxuan0811@hotmail.com>
2019-12-19 12:47:46 +0800

    bugfix

--- React.JS/blog-ts/src/pages/home/store/reducer.ts
+++ React.JS/blog-ts/src/pages/home/store/reducer.ts
@@ -9,9 +9,9 @@ const defaultState = fromJS({
 const reducer = (state = defaultState, action:HomeAction) => {
 	switch (action.type) {
 		case actionType.MoreArticleList:
-			return state.set('articleList',action.list);
-		case actionType.InitArticleList:
 			return state.set('articleList',state.get('articleList').concat(action.list));
+		case actionType.InitArticleList:
+			return state.set('articleList',action.list);
 		default:
 			return state;
 	}
