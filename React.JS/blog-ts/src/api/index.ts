5da879f64052b055a8faea1928c1efd6b4e51606
lijiaxuan <lijiaxuan0811@hotmail.com>
2020-04-13 20:06:26 +0800

    接口处理未登录初步完成

--- React.JS/blog-ts/src/api/index.ts
+++ React.JS/blog-ts/src/api/index.ts
@@ -1,28 +1,55 @@
+import {notification} from "antd";
+import axios from "axios";
 
 /**本地和线上两种环境的URL*/
 let API_URL = {
-	'development':'http://localhost:1016',
+	'development': 'http://localhost:1016',
 	// 'development':'https://blogserver.jiaxuanlee.com',
-	'production':'https://blogserver.jiaxuanlee.com'
+	'production': 'https://blogserver.jiaxuanlee.com'
 };
 
 let WS_URL = {
-	'development':'ws://localhost:1016',
+	'development': 'ws://localhost:1016',
 	// 'development':'wss://blogserver.jiaxuanlee.com',
-	'production':'wss://blogserver.jiaxuanlee.com'
+	'production': 'wss://blogserver.jiaxuanlee.com'
 };
 
 
 /**
  * @method 根据传入的路径获取最终请求的URL。
  * @param path string 传入的url路径*/
-export let GetURL =  (path:string) => {
-	return API_URL[process.env.NODE_ENV]+path;
+export let GetURL = (path: string) => {
+	return API_URL[process.env.NODE_ENV] + path;
 };
 
 /**
  * @method 根据传入的路径获取WebSocket的URL。
  * @param path string 传入的url路径*/
-export let GetWSURL = (path:string) => {
-	return WS_URL[process.env.NODE_ENV]+path;
+export let GetWSURL = (path: string) => {
+	return WS_URL[process.env.NODE_ENV] + path;
 };
+
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+	config.headers.leeKey = localStorage.getItem("leeKey");
+	config.headers.leeToken = localStorage.getItem("leeToken");
+	// 在发送请求之前做些什么
+	return config;
+}, function (error) {
+	// 对请求错误做些什么
+	return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+	console.log(response);
+	if (response.data.code === 1479) {
+		notification.error({description:'即将跳转首页', message: response.data.data,onClose:() => {window.location.href = "/"}});
+	}
+	// 对响应数据做点什么
+	return response;
+}, function (error) {
+	// 对响应错误做点什么
+	return Promise.reject(error);
+});
