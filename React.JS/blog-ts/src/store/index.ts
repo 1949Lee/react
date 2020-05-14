551c6b45c71ed712b3cbb97cf4fe9a3804872032
1949Lee <lijiaxuan0811@hotmail.com>
2020-05-14 08:40:22 +0800

    bugfix

--- React.JS/blog-ts/src/store/index.ts
+++ React.JS/blog-ts/src/store/index.ts
@@ -5,11 +5,18 @@ import * as actionTypes from './action-type';
 import thunk from "redux-thunk";
 import {Map} from "immutable";
 
-// @ts-ignore
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // const composeEnhancers = compose;
-const store:Store<Map<string,any>> = createStore(reducer, /* preloadedState, */ composeEnhancers(
-	applyMiddleware(thunk)
-));
-export {actionCreators,actionTypes};
+let store: Store<Map<string, any>>;
+if (process.env.NODE_ENV === 'production') {
+	store = createStore(reducer, /* preloadedState, */ compose<any>(
+		applyMiddleware(thunk)
+	));
+} else {
+	// @ts-ignore
+	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+	store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+		applyMiddleware(thunk)
+	));
+}
+export {actionCreators, actionTypes};
 export default store;
